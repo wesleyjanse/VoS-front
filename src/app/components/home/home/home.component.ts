c8a3236ad83f982dc5c8e489816a3dc652849fab
wesleyjanse <wesleyjanse@gmail.com>
2020-01-22 14:55:50 +0100

    homepage done

--- src/app/components/home/home/home.component.ts
+++ src/app/components/home/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as Chart from 'chart.js'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,6 +8,34 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+  canvas: any;
+  ctx: any;
+
+  ngAfterViewInit() {
+    this.canvas = document.getElementById('myChart');
+    this.ctx = this.canvas.getContext('2d');
+    let myChart = new Chart(this.ctx, {
+      type: 'bar',
+      data: {
+        labels: ["Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "Oktober", "November", "December"],
+        datasets: [{
+          label: "Aantal overtredingen",
+          data: [10, 5, 4, 8, 2, 0, 7, 4, 6, 7, 3, 1],
+          backgroundColor: '#B668B7',
+          borderWidth: 0,
+        }]
+      },
+
+      options: {
+        responsive: true,
+        display: true,
+        maintainAspectRatio: false
+      }
+    });
+  }
+
+
+
 
   ngOnInit() {
   }
